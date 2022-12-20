ba37e36bf6d8b4ba09307beea547ed0e439722e1
rudra <rudra8997@gmail.com>
2022-12-20 12:49:34 +0000

    addDayyX

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const addDays = require("date-fns/addDays");
 
 function addAfterDays(days) {
-  return addDays(new Date("22-aug-2020"), days);
-}
-//let date = addAfterDays(days);
-//let newdateformat =
-//date.getDate() + "-" + date.getMonth() + "-" + date.getFullYear();
+  const date = addDays(new Date("22-aug-2020"), days);
 
+  let newdateformat =
+    date.getDate() + "-" + (date.getMonth() + 1) + "-" + date.getFullYear();
+  return newdateformat;
+}
+console.log(addAfterDays(3));
 module.exports = addAfterDays;
